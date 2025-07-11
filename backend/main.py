91cba132beff3bf6f5661da2b30eff5c28164bee
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-07-11 16:57:57 -0600

    Endpoints lecciones

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-from backend.routes import auth_router, user_router, category_level_router
+from backend.routes import auth_router, user_router, category_level_router, lessons_router
 
 app = FastAPI(title="GamifyPy")
 
@@ -18,6 +18,7 @@ app.add_middleware(
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(category_level_router, prefix="/category-level", tags=["category_level"])
+app.include_router(lessons_router, prefix="/lessons", tags=["lessons"])
 app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
 if __name__ == "__main__":
--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,9 +1,11 @@
 from .auth import router as auth_router
 from .user import router as user_router
 from .category_level import router as category_level_router
+from .lessons import router as lessons_router
 
 __all__ = [
     "auth_router",
     "user_router",
-    "category_level_router"
+    "category_level_router",
+    "lessons_router"
 ]
