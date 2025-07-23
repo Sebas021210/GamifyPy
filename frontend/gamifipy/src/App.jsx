79941a4e342d13cb2e2932a14b68cd5db135aacd
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-07-23 01:04:28 -0600

    Configuración rutas

--- frontend/gamifipy/src/App.jsx
+++ frontend/gamifipy/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './view/home/home.jsx'
 import './App.css'
 
@@ -6,7 +7,11 @@ function App() {
   return (
     <>
       <div>
-        < Home />
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Router>
       </div>
     </>
   )
