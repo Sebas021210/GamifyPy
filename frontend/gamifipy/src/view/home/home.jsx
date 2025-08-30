7a049230c5263c034f8fbcd5f39cf366436e49dc
Sebastián Solorzano <sol21826@uvg.edu.gt>
2025-08-29 22:37:56 -0600

    Ajustes

--- frontend/gamifipy/src/view/home/home.jsx
+++ frontend/gamifipy/src/view/home/home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import './Home.css';
+import './home.css';
 
 function Home() {
     const navigate = useNavigate();
